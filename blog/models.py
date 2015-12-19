f1fd1e50bcbdfc3e4c3bdb829aad63b517b96741
Alex <amai2@u.rochester.edu>
2015-12-18 21:52:03 -0500

    My Django app, first commit

---
+++ blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils import timezone
+
+#class defines our model, an object
+#models.Model means Post object is a Django Model, so Django knows it should be saved in db
+class Post(models.Model):
+	author = models.ForeignKey('auth.User')
+	title = models.CharField(max_length=200)
+	text = models.TextField()
+	created_date = models.DateTimeField(default=timezone.now)
+	published_date = models.DateTimeField(blank=True, null=True)
+
+	def publish(self):
+		self.published_date = timezone.now()
+		self.save()
+
+	def __str__(self):
+		return self.title
